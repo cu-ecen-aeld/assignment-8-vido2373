794134c2560abe431e089bf8dbd7726602011842
Vishnu Dodballapur <vido2373@colorado.edu>
2021-10-25 18:39:13 -0600

    passing full-test.sh

--- base_external/package/aesd-device-driver/aesd-device-driver.mk
+++ base_external/package/aesd-device-driver/aesd-device-driver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_DEVICE_DRIVER_VERSION = eaec79e62829b1c304d93550157a0577f4ff4589
+AESD_DEVICE_DRIVER_VERSION = edd612dd3272c9cf1808d9af1e5209ffbcc7900a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
